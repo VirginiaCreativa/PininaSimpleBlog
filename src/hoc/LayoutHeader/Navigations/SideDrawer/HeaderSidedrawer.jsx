30dce08507a92c1b5f91ee4f13553e2b1a5892f3
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-09-28 11:35:27 -0500

    HeaderSideDrawer from Responsive Fin

--- src/hoc/LayoutHeader/Navigations/SideDrawer/HeaderSidedrawer.jsx
+++ src/hoc/LayoutHeader/Navigations/SideDrawer/HeaderSidedrawer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import bootstrapGrid from 'bootstrap/scss/bootstrap-grid.scss';
 import Aux from '../../../Aux/Aux';
 import Logo from '../../Logo/Logo';
 import SideDrawer from './SideDrawer';
@@ -6,7 +7,14 @@ import classes from './HeaderSiderawer.scss';
 
 const HeaderSidedrawer = () => (
   <Aux>
-    <div className={classes.HeaderSidedrawer}>
+    <div
+      className={[
+        classes.HeaderSidedrawer,
+        bootstrapGrid['d-block'],
+        bootstrapGrid['d-sm-auto'],
+        bootstrapGrid['d-md-none'],
+      ].join(' ')}
+    >
       <Logo sizeLogo="4.75" alignLogo="center">
         <div className={classes.Navbar}>
           <img
