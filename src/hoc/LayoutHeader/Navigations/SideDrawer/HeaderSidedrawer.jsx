950862f89e383a73db96d76bbc000d24fef0aba9
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-10-01 16:13:41 -0500

    Navbar Icon Closed

--- src/hoc/LayoutHeader/Navigations/SideDrawer/HeaderSidedrawer.jsx
+++ src/hoc/LayoutHeader/Navigations/SideDrawer/HeaderSidedrawer.jsx
@@ -5,27 +5,36 @@ import Logo from '../../Logo/Logo';
 import SideDrawer from './SideDrawer';
 import classes from './HeaderSiderawer.scss';
 
-const HeaderSidedrawer = ({ onShow, toggleNavbar, onClosed, onSideout }) => (
-  <Aux>
-    <div
-      className={[
-        classes.HeaderSidedrawer,
-        bootstrapGrid['d-block'],
-        bootstrapGrid['d-sm-auto'],
-        bootstrapGrid['d-md-none'],
-      ].join(' ')}
-    >
-      <Logo sizeLogo="4.75" alignLogo="center">
-        <div className={classes.Navbar}>
-          <button className={classes.btnMenu} type="button" onClick={onShow} />
-        </div>
-      </Logo>
-      <SideDrawer
-        toggleNavbar={toggleNavbar}
-        onClosed={onClosed}
-        onSideout={onSideout}
-      />
-    </div>
-  </Aux>
-);
+const HeaderSidedrawer = ({ onShow, toggleNavbar, onClosed, onSideout }) => {
+  let toggleMenu = [classes.btnMenu];
+  if (toggleNavbar) toggleMenu = [classes.btnMenu, classes.On].join(' ');
+  return (
+    <Aux>
+      <div
+        className={[
+          classes.HeaderSidedrawer,
+          bootstrapGrid['d-block'],
+          bootstrapGrid['d-sm-auto'],
+          bootstrapGrid['d-md-none'],
+        ].join(' ')}
+      >
+        <Logo sizeLogo="4.75" alignLogo="center">
+          <div
+            className={classes.Navbar}
+            style={{ zIndex: toggleNavbar ? '400' : '100' }}
+          >
+            <button className={toggleMenu} type="button" onClick={onShow}>
+              <span />
+            </button>
+          </div>
+        </Logo>
+        <SideDrawer
+          toggleNavbar={toggleNavbar}
+          onClosed={onClosed}
+          onSideout={onSideout}
+        />
+      </div>
+    </Aux>
+  );
+};
 export default HeaderSidedrawer;
